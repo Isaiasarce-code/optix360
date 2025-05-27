27a6e21bb564da42cac199cbadabd2877a2936da
Isaiasarce-code <isaiasarce321@gmail.com>
2025-05-27 09:41:06 -0600

    Update optix360.py

--- optix360.py
+++ optix360.py
@@ -18,7 +18,7 @@ st.markdown("""
             color: white;
         }
         .stApp {
-            background-color: #E5E7EB;
+            background-color: #1E3A8A;
             color: white;
         }
         .css-18ni7ap.e8zbici2,  /* Títulos y textos */
