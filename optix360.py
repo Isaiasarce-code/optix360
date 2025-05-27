b8c407557f5c1261518d75b9cd36a7cb47ea0f15
Isaiasarce-code <isaiasarce321@gmail.com>
2025-05-27 10:36:42 -0600

    Update optix360.py

--- optix360.py
+++ optix360.py
@@ -21,7 +21,7 @@ st.markdown("""
 
         /* Textos y encabezados */
         h1, h2, h3, h4, h5, h6, p, span, div {
-            color: #FFFFFF !important;
+            color: #90ee90 !important;
         }
 
         /* Subheaders y textos específicos */
